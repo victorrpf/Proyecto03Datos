672d3aff4cb6a78a7743af9d3744235b103787e5
victorrpf <victor.rpf@gmail.com>
2023-03-04 02:00:44 +0100

    maxi mini false

--- fmAcercade.designer.cs
+++ fmAcercade.designer.cs
@@ -82,6 +82,8 @@
             this.Controls.Add(this.btAceptar);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "fmAcercade";
             this.Text = "fmAcercade";
             this.Load += new System.EventHandler(this.fmAcercade_Load);
